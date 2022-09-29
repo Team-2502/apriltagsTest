5a0da7fcd99e3d5349660e17af0d6d42dbad484a
90307528 <90307528@hs-34593-macair.ad.edenpr.org>
2022-09-28 20:29:02 -0500

    added diffs for performance increase

--- liveAprilTags.py
+++ liveAprilTags.py
@@ -7,11 +7,15 @@ import cv2
 
 vid = cv2.VideoCapture(0)
 
+vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1)
+vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1)
+
 while(True):
 
 	ret, frame = vid.read()
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	options = apriltag.DetectorOptions(families="tag36h11")
+	options = apriltag.DetectorOptions(families="tag36h11", quad_decimate=5.0,
+            nthreads=4)
 	detector = apriltag.Detector(options)
 	results = detector.detect(gray)
 	# loop over the AprilTag detection results
